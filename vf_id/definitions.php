b49e1601719c1ef943bac522fdcff8e4f0e18e59
andhra21231 <andhra.rifqi@gmail.com>
2022-02-07 15:03:38 +0700

    add dash core language syntax

---
+++ vf_id/definitions.php
@@ -0,0 +1,16 @@
+<?php
+
+$LocaleInfo['vf_id'] = array (
+    'Locale' => 'id',
+    'Name' => 'Indonesia',
+    'EnName' => 'Indonesian',
+    'Description' => 'Recode Indonesia Language',
+    'Version' => '2022.02.07',
+    'Author' => 'Pelajar Social',
+    'AuthorUrl' => 'https://pelajar.social',
+    'License' => 'MIT License',
+    'PercentComplete' => 100,
+    'NumComplete' => 3374,
+    'DenComplete' => 3374,
+    'Icon' => 'id.svg',
+);
